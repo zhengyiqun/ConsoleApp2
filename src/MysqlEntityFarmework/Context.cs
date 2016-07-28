874423a15b50425f020fe086b7e74c2e1b83a86c
zhengyiqun <zheng_yiqun@hotmail.com>
2016-07-28 17:52:08 +0800

    修改了项目结构

---
+++ src/MysqlEntityFarmework/Context.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace MysqlEntityFarmework
+{
+    public class MyContext:DbContext{ }
+}
